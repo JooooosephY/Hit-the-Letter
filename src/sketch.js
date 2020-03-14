b7cddc00513659258e7be1b6041d9eadd734f846
Zhao Yang <yangzhao7508@outlook.com>
2020-03-14 13:45:38 +0800

    some improvement

--- src/sketch.js
+++ src/sketch.js
@@ -60,6 +60,34 @@ function drawPoses(poses) {
   // console.log(poses);
   console.log(model);
   
+// When the confidence score is more than 0.5, use rightWrist as the parameter
+if (poses.length > 0){
+  if (poses[0].pose.rightWrist.confidence > 0.5){
+    rwy = poses[0].pose.rightWrist.y;
+    rwx = width - poses[0].pose.rightWrist.x;
+  }else{
+    rwx = mouseX;
+    rwy = mouseY;
+  }
+} else{
+  rwx = mouseX;
+  rwy = mouseY;
+}
+
+// When the confidence score is more than 0.5, use leftWrist as the parameter
+if (poses.length > 0){
+  if (poses[0].pose.leftWrist.confidence > 0.5){
+    lwy = poses[0].pose.leftWrist.y;
+    lwx = width - poses[0].pose.leftWrist.x;
+  }else{
+    lwx = width;
+    lwy = height;
+  }
+} else{
+  lwx = width;
+  lwy = height;
+}
+
   // Modify the graphics context to flip all remaining drawing horizontally.
   // This makes the image act like a mirror (reversing left and right); this is
   // easier to work with.
@@ -74,37 +102,15 @@ function drawPoses(poses) {
   }
   
   
-  drawKeypoints(poses);
-  drawSkeleton(poses);
+  // drawKeypoints(poses);
+  // drawSkeleton(poses);
+  
   pop();
 
-  // When the confidence score is more than 0.5, use rightWrist as the parameter
-  if (poses.length > 0){
-    if (poses[0].pose.rightWrist.confidence > 0.5){
-      rwy = poses[0].pose.rightWrist.y;
-      rwx = width - poses[0].pose.rightWrist.x;
-    }else{
-      rwx = mouseX;
-      rwy = mouseY;
-    }
-  } else{
-    rwx = mouseX;
-    rwy = mouseY;
-  }
+  wrist(rwx, rwy, 30);
+  wrist(lwx, lwy, 30);
 
-  if (poses.length > 0){
-    if (poses[0].pose.leftWrist.confidence > 0.5){
-      lwy = poses[0].pose.leftWrist.y;
-      lwx = width - poses[0].pose.leftWrist.x;
-    }else{
-      lwx = 0;
-      lwy = 0;
-    }
-  } else{
-    lwx = 0;
-    lwy = 0;
-  }
-
+  
   let sizey0 = (height/3)+height/5;
   let sizey1 = (height/3)+1.7*height/5;
   let sizey2 = (height/3)+2.5*height/5;
@@ -115,9 +121,11 @@ function drawPoses(poses) {
     case 0:
       // Display the Start, Settings and Exit
       fill(255);
-      textSize(int(width/10));
       textAlign(CENTER, CENTER);
       textFont(tf);
+      textSize(30);
+      text("Hins: Move your right wrist to choose.", width/2, 20);
+      textSize(int(width/10));
       text("Hit the Letter!", width/2, height/3);      
       textSize(width/25);
       text("-Start-", width/2, sizey0);
@@ -200,12 +208,10 @@ function drawPoses(poses) {
     case 1:
       // Hint
       fill(255);
-      textSize(25);
+      textSize(30);
       text("This is the letter that you should hit now: " + alphabet[index], width/2, 20 );
 
       // Time left
-      fill(255);
-      textSize(30);
       text("Time left: " + time, width / 16, 20);
       if (time_count == 0){
         time --;
@@ -216,9 +222,12 @@ function drawPoses(poses) {
         time_count = 0;
       }
 
+      // Score
+      text("Score: " + score, width * 15/16, 20);
+
       // generate 1 letter per 5 frames
       if (count == 0){
-        letters.push( new Letter(random(width), random(0, -100), alphabet[int(random(index,index + 6))]) );
+        letters.push( new Letter(random(width), random(0, -50), alphabet[int(random(index,index + 4))]) );
         count ++;
       }else if (count < 5){
         count ++;
@@ -229,22 +238,22 @@ function drawPoses(poses) {
       // update and display the letters
       for (let i=0; i<letters.length; i++) {
         let l = letters[i];
-        l.move();
-        l.updateLifespan();
+        l.update();
         l.display();
+        l.move();
       }
       
       
       // detect whether you hit the letter
       for (let i = letters.length-1; i >= 0; i--) {
-        if (rwx < letters[i].x + letters[i].size / 2 && rwx > letters[i].x - letters[i].size / 2 && rwy < letters[i].y + letters[i].size / 2 && rwx > letters[i].x - letters[i].size / 2){
+        if (rwx < letters[i].x + letters[i].size / 2 && rwx > letters[i].x - letters[i].size / 2 && rwy < letters[i].y + letters[i].size / 2 && rwy > letters[i].y - letters[i].size / 2){
           if (letters[i].txt == alphabet[index]){
             letters[i].hit();
             letters.splice(i, 1);
             score += 100;
             index ++;
           }
-        } else if (lwx < letters[i].x + letters[i].size / 2 && lwx > letters[i].x - letters[i].size / 2 && lwy < letters[i].y + letters[i].size / 2 && lwx > letters[i].x - letters[i].size / 2){
+        } else if (lwx < letters[i].x + letters[i].size / 2 && lwx > letters[i].x - letters[i].size / 2 && lwy < letters[i].y + letters[i].size / 2 && lwy > letters[i].y - letters[i].size / 2){
           if (letters[i].txt == alphabet[index]){
             letters[i].hit();
             letters.splice(i, 1);
@@ -276,6 +285,10 @@ function drawPoses(poses) {
         }
       }
       
+      if (time < 0){
+        ca = 2;
+      }
+
       if (frameCount - frm > 3350){
         ca = 2;
       }
@@ -300,7 +313,7 @@ function drawPoses(poses) {
       textFont('Georgia')
       text("Score: " + score, width/2, sizey0);
       textFont(tf);
-      text("-Restart-", width/2, sizey1);
+      text("-Back to Menu-", width/2, sizey1);
       text("-Exit-", width/2, sizey2);
       
       // The effect of putting your hand on the Start bar
@@ -318,9 +331,8 @@ function drawPoses(poses) {
         if (remaining < 160) { // less than 4 seconds, display progress bar
           fill(255);
           arc(rwx, rwy, 80, 80, 0, radians(map(remaining, 0, 159, 0, 360)), PIE);
-        } else { // Restart the game
-          rwy = 100;
-          ca = 1;
+        } else { // Back to Menu
+          ca = 0;
           initialize();
           goin_b1 = true;
         }
@@ -456,30 +468,10 @@ function drawPoses(poses) {
   // text( frameRate(), 80, 20 );
 }
 
-// Draw ellipses over the detected keypoints
-function drawKeypoints(poses) {
-  poses.forEach(pose =>
-    pose.pose.keypoints.forEach(keypoint => {
-      if (keypoint.score > 0.2) {
-        fill(0, 255, 0);
-        noStroke();
-        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
-      }
-    })
-  );
-}
-
-// Draw connections between the skeleton joints.
-function drawSkeleton(poses) {
-  poses.forEach(pose => {
-    pose.skeleton.forEach(skeleton => {
-      // skeleton is an array of two keypoints. Extract the keypoints.
-      const [p1, p2] = skeleton;
-      stroke(255, 255, 0);
-      strokeWeight(5);
-      line(p1.position.x, p1.position.y, p2.position.x, p2.position.y);
-    });
-  });
+function wrist(x, y, size){
+  fill(map(x, 0, width, 0, 255), map(y, 0, height, 0 ,255), map(x + y, 0, width+height, 0,255));
+  noStroke();
+  ellipse(x, y, size, size);
 }
 
 // Initialize the game
@@ -489,8 +481,34 @@ function initialize(){
   letters = [];
   frm = frameCount;
   index = 0; 
+  time = 60;
 }
 
+// Draw ellipses over the detected keypoints
+// function drawKeypoints(poses) {
+//   poses.forEach(pose =>
+//     pose.pose.keypoints.forEach(keypoint => {
+//       if (keypoint.score > 0.2) {
+//         fill(0, 255, 0);
+//         noStroke();
+//         ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+//       }
+//     })
+//   );
+// }
+
+// // Draw connections between the skeleton joints.
+// function drawSkeleton(poses) {
+//   poses.forEach(pose => {
+//     pose.skeleton.forEach(skeleton => {
+//       // skeleton is an array of two keypoints. Extract the keypoints.
+//       const [p1, p2] = skeleton;
+//       stroke(255, 255, 0);
+//       strokeWeight(5);
+//       line(p1.position.x, p1.position.y, p2.position.x, p2.position.y);
+//     });
+//   });
+// }
 
 
 // Letter class
@@ -501,30 +519,40 @@ class Letter {
     this.y = y;
     this.size = 45;
     this.xSpd = 0;
-    this.ySpd = random(7, 13);
+    this.ySpd = random(7, 11);
     this.r = 255;
     this.g = 255;
     this.b = 0;
 
     this.txt = txt; 
-    // Lifespan of the firework
-    this.lifespan = 1.0; // 100%
-    this.lifeReduction = random(0.008, 0.015);
     this.isDone = false;
   }
   // methods
 
-  updateLifespan() {
-    this.lifespan -= this.lifeReduction;
-    if (this.lifespan <= 0.0) {
+  update() {
+    if (score >= 1000 && score < 2000){
+      this.xSpd = random(-10, 10);
+      this.ySpd = random(11, 15);
+    }else if (score >= 2000){
+      this.xSpd = random(-20,20);
+      this.ySpd = random (15, 20);
+    }
+
+    if (this.y >= height) {
       this.isDone = true;
     }
   }
 
   // move the letter
   move() {
-    this.x += this.xSpd;
+    this.x -= this.xSpd;
     this.y += this.ySpd;
+
+    if (this.x < 0){
+      this.xSpd = - this.xSpd;
+    }else if (this.x > width){
+      this.xSpd = - this.xSpd;
+    }
   }
   
   // display the letter
@@ -547,7 +575,7 @@ class Letter {
   }
 }
 
-// Explosion class
+// Hit class
 class Hit {
   constructor(x, y){
     this.x = x;
